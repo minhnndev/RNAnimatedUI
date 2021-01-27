d826ff5c11daec7862c7bcd316d8c148e4fcfaf6
MinhNNDev <ngocminhit2000@gmail.com>
2021-01-27 23:28:35 +0700

    Init ProjectUI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import AppExport from './project/animatedTabs';
+
+const App = () => {
+  return <AppExport />;
+};
+
+export default App;
